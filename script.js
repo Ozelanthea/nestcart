6c9176494882fdd27dbfd7a3271b275b8f73c945
Ozelanthea <ozelanthea@gmail.com>
2026-08-04 18:13:23 +0100

    Wire up Add to Cart with localStorage and live cart count badge

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const navMenu = document.querySelector("nav ul");
 const productGrid = document.querySelector(".product-grid");
+const cartCount = document.querySelector(".cart-count");
+let allProducts = [];
 
 hamburgerBtn.addEventListener("click", () => {
     navMenu.classList.toggle('nav-open')
 
+})
+
+productGrid.addEventListener("click", (e) => {
+    if(e.target.classList.contains("add-to-cart-btn")) {
+        let idValue = Number(e.target.dataset.id);
+
+        let product = allProducts.find(item => item.id === idValue);
+
+        addToCart(product);
+    }
 })
 
 fetch("https://fakestoreapi.com/products").then(response => response.json()).then(data => renderProducts(data));
@@ -21,9 +33,47 @@ function createProductCard(product) {
 }
 
 function renderProducts(data) {
+    allProducts = data;
     const cardsHTML = data.slice(0, 8).map(createProductCard).join("");
     productGrid.innerHTML = cardsHTML;
 }
+
+function getCart() {
+    let cartData = localStorage.getItem("cart");
+    if (cartData !== null) {
+        return JSON.parse(cartData);
+    } else {
+        return [];
+    }
+}
+
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+function addToCart(product) {
+    let cart = getCart();
+    let existingItem = cart.find(item => item.id === product.id);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push(
+            {id: product.id, title: product.title, price: product.price, image: product.image, quantity: 1}
+        );
+    }
+
+    saveCart(cart);
+    updateCartCount();
+}
+
+function updateCartCount() {
+    let cart = getCart();
+    let total = cart.reduce((total, item) => total + item.quantity, 0);
+    cartCount.textContent = total;
+}
+
+updateCartCount();
 
 
 
